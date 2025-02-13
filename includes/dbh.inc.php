52732511140596c0283b39cb451c7c56b08d68ab
Othmane benchekroun <Othmanebenchekroun23@gmail.com>
2025-02-13 10:55:52 +0100

    Connected the app with the database and structured the files

---
+++ includes/dbh.inc.php
@@ -0,0 +1,16 @@
+<?php
+
+$dsn = "mysql:host=localhost;dbname=schulbibliothek";
+$dbusername = "root";
+$dbpassword = "";
+
+try{
+    $pdo = new PDO($dsn, $dbusername, $dbpassword);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // in case of an error this makes an exception so we can grab it in handle it in catch
+} catch(PDOException $e){
+  
+    echo "Connection failed: " . $e->getMessage();
+    die();
+
+}
